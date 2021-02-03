1824297389c109d68c26e18467cb1b5a8c8cd6fc
Mitesh Mittal <miteshmittal22@gmail.com>
2021-02-03 01:26:04 +0000

    Update ChargingStationAssignment.java

--- src/main/java/de/fzi/efeu/service/ChargingStationAssignment.java
+++ src/main/java/de/fzi/efeu/service/ChargingStationAssignment.java
@@ -1,94 +1,68 @@
 package de.fzi.efeu.service;
 
+
 import de.fzi.efeu.efeuportal.ApiException;
-import de.fzi.efeu.efeuportal.api.*;
-import de.fzi.efeu.efeuportal.model.*;
+import de.fzi.efeu.efeuportal.api.ChargingStationApi;
+import de.fzi.efeu.efeuportal.api.ProcessMgmtApi;
+import de.fzi.efeu.efeuportal.api.VehicleApi;
+import de.fzi.efeu.efeuportal.model.EfCaChargingStation;
+import de.fzi.efeu.efeuportal.model.EfCaVehicle;
+
+import de.fzi.efeu.efeuportal.model.VehicleStatus;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Service;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
 
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-@Service
-//@SpringBootApplication
-public class ChargingStationAssignment {
+
+import static org.junit.Assert.*;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+
+public class ChargingStationAssignmentTest {
+    @Autowired
+    private ChargingStationAssignment chargingStationAssignment;
+
+    //@MockBean
     @Autowired
     private VehicleApi vehicleApi;
 
     @Autowired
-    private ChargingStationApi chargingStationApi;
-
-    @Autowired
     private ProcessMgmtApi processMgmtApi;
 
-    // Vehicle-Station-Assignment: Vehicle i to Station i
-    //TODO: Fahrzeug zur nahsten Ladestation
+    //@MockBean
+    @Autowired
+    private ChargingStationApi chargingStationApi;
 
-    public Map<String, String> assignVehicleToStation() throws ApiException {
-        Map<String, String> mapVehicleStation = new HashMap<>();
-        List<EfCaVehicle> vehicles = vehicleApi.getAllVehicles().getVehicles();
+    @Test
+    public void testChargingStationAssignment() throws ApiException {
 
-        List<EfCaChargingStation> chargingStations = chargingStationApi.getAllChargingStations().getChargingStations();
-        for (int i = 0; i < vehicles.size(); i++) {
-            mapVehicleStation.put(vehicles.get(i).getIdent(), chargingStations.get(i).getIdent());
-        }
-        return mapVehicleStation;
+        EfCaChargingStation chargingStation1 = new EfCaChargingStation().ident("S1").latitude(49.116625).longitude(8.588302);
+        EfCaChargingStation chargingStation2 = new EfCaChargingStation().ident("S2").latitude(49.117478).longitude(8.590465);
+        EfCaChargingStation chargingStation3 = new EfCaChargingStation().ident("S3").latitude(49.116569).longitude(8.590589);;
+        EfCaChargingStation chargingStation4 = new EfCaChargingStation().ident("S4").latitude(49.116422).longitude(8.590137);;
+
+        List<EfCaChargingStation> chargingStations = List.of(chargingStation1, chargingStation2, chargingStation3, chargingStation4);
+
+        VehicleStatus vehicle1;
+
+        vehicle1 = new VehicleStatus();
+        vehicle1.setLatitude((float) 41.62);
+        vehicle1.setLongitude((float) 8.78);
+
+        EfCaChargingStation nearestToVehicle1 = chargingStationAssignment.assignVehicleToStation(vehicle1);
+
+
+
+        assertEquals("S4", nearestToVehicle1.getIdent());
+
     }
-
-    public Map<EfCaChargingStation, Double> assignVehicleToStation(EfCaVehicle vehicle) throws ApiException {
-        System.out.println("Hello");
-        List<EfCaVehicle> vehicles = vehicleApi.getAllVehicles().getVehicles();
-
-        List<EfCaChargingStation> chargingStations = chargingStationApi.getAllChargingStations().getChargingStations();
-        //Hashmap<vehicles,Integer> vehiclemap = null;
-        //private Map<String, Duration> offsetByVehicle = new HashMap<>()
-         Map<String,Integer> vehiclemap = new HashMap<>();
-        //List<Integer> listlat = null;
-        Float lat_veh = processMgmtApi.getVehicleStatus(vehicle.getIdent()).getLatitude();
-
-
-        Float lon_veh = processMgmtApi.getVehicleStatus(vehicle.getIdent()).getLongitude();
-
-        Map<EfCaChargingStation, Double>  distMap= new HashMap<>();
-        for(int j=0; j<=chargingStations.size(); j++){
-
-
-            double lat_cs = chargingStations.get(j).getLatitude();
-            double lon_cs = chargingStations.get(j).getLongitude();
-
-            double theta_lat = lat_cs - lat_veh;
-            double theta_lon = lon_cs - lon_veh;
-
-            double distance = 3;
-            distMap.put(chargingStations.get(j),distance );
-
-           // return distMap;
-
-           //double theta = lon1 - lon2;
-           //double dist = Math.sin(deg2rad(lat1)) * Math.sin(deg2rad(lat2))
-              //      + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2))
-              //      * Math.cos(deg2rad(theta));
-            //dist = Math.acos(dist);
-           // dist = rad2deg(dist);
-           // dist = dist * 60; // 60 nautical miles per degree of sepera
-
-
-            //String std = chargingStationApi.getAllChargingStations().getChargingStations().
-            //System.out.println(j);
-
-        }
-
-
-            // return map
-        return distMap;
-    }
-
-
-    //public String getAssignedStation(EfCaVehicle vehicle) throws ApiException {
-       // Map<String, String> mapVehicleStation = assignVehicleToStation();
-      //  return mapVehicleStation.get(vehicle.getIdent());
-   // }
 
 }
+
+
+
